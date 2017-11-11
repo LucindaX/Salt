f7ca170b86b8b17ebee68d900a256a1013dc57b1
Ahmed Osman <ahmedosmanazrk@gmail.com>
2017-11-12 01:09:31 +0200

    added schema validation to uploaded resumes

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,38 @@
-$("#resume-upload").on("change", function(){
-  $("#main-container").html("");
-  var file = $(this).prop('files')[0];
-  var fr = new FileReader();
-  fr.onload = function(e){
-	  var resumeObject = JSON.parse(e.target.result);
-    var resume = new Resume(resumeObject);
-    resume.build();
-	  $("#main-container").html(resume.html);
-  }
-	fr.readAsBinaryString(file);
+$(function(){
+  
+  $.getJSON("/data/schema.json", function(data){
+    
+    var ajv = new Ajv();
+    var validate = ajv.compile(data);
+    
+    $("#resume-upload").on("change", function(){
+
+      $("#main-container").html("");
+  
+      var file = $(this).prop('files')[0];
+      if (file.type != "application/json") return alert("upload .json extentions only");
+    
+      var fr = new FileReader();
+    
+      fr.onload = function(e){
+	  
+        var resumeObject = JSON.parse(e.target.result);
+
+        if (!validate(resumeObject)){
+          console.log(validate.errors);
+          return alert("your resume json format doesn't follow accepted schema");
+        }
+
+         var resume = new Resume(resumeObject);
+        resume.build();
+	      $("#main-container").html(resume.html);
+     
+       }
+	    
+      fr.readAsBinaryString(file);
+    
+    });
+
+  });
+
 })
